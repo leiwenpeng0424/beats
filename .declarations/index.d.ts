eb0afe29976eab42f8a20110e1eccb63e169181d
leiwenpeng <leiwenpeng1994@outlook.com>
2023-07-08 19:39:41 +0800

    Fix TS error

--- .declarations/index.d.ts
+++ .declarations/index.d.ts
@@ -1,3 +1,4 @@
 import "./cli";
 import type { Config } from "./configuration";
+export * from "./configuration";
 export declare function defineConfig(options: Config): Config;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "./cli";
 import type { Config } from "./configuration";
 
+export * from "./configuration";
+
 export function defineConfig(options: Config) {
     return options;
 }
